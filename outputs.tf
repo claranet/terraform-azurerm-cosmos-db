351dffc16c3f7350967fd4c864732a3cb5de8a5a
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-11-20 14:07:36 +0100

    AZ-79: README, outputs and desc updates

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,54 @@
-output "id" {
-  value = azurerm_cosmosdb_account.db.id
+output "cosmosdb_id" {
+  description = "The CosmosDB Account ID."
+  value       = azurerm_cosmosdb_account.db.id
 }
 
-output "name" {
-  value = azurerm_cosmosdb_account.db.name
+output "cosmosdb_name" {
+  description = "The CosmosDB Account Name."
+  value       = azurerm_cosmosdb_account.db.name
 }
 
-output "endpoint" {
-  value = azurerm_cosmosdb_account.db.endpoint
+output "cosmosdb_endpoint" {
+  description = "The endpoint used to connect to the CosmosDB account."
+  value       = azurerm_cosmosdb_account.db.endpoint
 }
 
-output "read_endpoints" {
-  value = azurerm_cosmosdb_account.db.read_endpoints
+output "cosmosdb_read_endpoints" {
+  description = "A list of read endpoints available for this CosmosDB account."
+  value       = azurerm_cosmosdb_account.db.read_endpoints
 }
 
-output "write_endpoints" {
-  value = azurerm_cosmosdb_account.db.write_endpoints
+output "cosmosdb_write_endpoints" {
+  description = "A list of write endpoints available for this CosmosDB account."
+  value       = azurerm_cosmosdb_account.db.write_endpoints
 }
 
-output "primary_master_key" {
-  value = azurerm_cosmosdb_account.db.primary_master_key
+output "cosmosdb_primary_master_key" {
+  description = "The Primary master key for the CosmosDB Account."
+  value       = azurerm_cosmosdb_account.db.primary_master_key
+  sensitive   = true
 }
 
-output "secondary_master_key" {
-  value = azurerm_cosmosdb_account.db.secondary_master_key
+output "cosmosdb_secondary_master_key" {
+  description = " The Secondary master key for the CosmosDB Account."
+  value       = azurerm_cosmosdb_account.db.secondary_master_key
+  sensitive   = true
 }
 
-output "primary_readonly_master_key" {
-  value = azurerm_cosmosdb_account.db.primary_readonly_master_key
+output "cosmosdb_primary_readonly_master_key" {
+  description = "The Primary read-only master Key for the CosmosDB Account."
+  value       = azurerm_cosmosdb_account.db.primary_readonly_master_key
+  sensitive   = true
 }
 
-output "secondary_readonly_master_key" {
-  value = azurerm_cosmosdb_account.db.secondary_readonly_master_key
+output "cosmosdb_secondary_readonly_master_key" {
+  description = "The Secondary read-only master key for the CosmosDB Account."
+  value       = azurerm_cosmosdb_account.db.secondary_readonly_master_key
+  sensitive   = true
 }
 
-output "connection_strings" {
-  value = azurerm_cosmosdb_account.db.primary_readonly_master_key
+output "cosmosdb_connection_strings" {
+  description = "A list of connection strings available for this CosmosDB account."
+  value       = azurerm_cosmosdb_account.db.primary_readonly_master_key
+  sensitive   = true
 }
