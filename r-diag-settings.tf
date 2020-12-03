a4bef830d2bc755a15fc635fb659008029103951
Jérôme Respaut <jerome.respaut@fr.clara.net>
2020-12-03 15:29:16 +0100

    AZ-79: Update diag to v4.0.1

--- r-diag-settings.tf
+++ r-diag-settings.tf
@@ -2,7 +2,7 @@ module "logging" {
   count = var.logs_destinations_ids != [] ? 1 : 0
 
   source  = "claranet/diagnostic-settings/azurerm"
-  version = "4.0.0"
+  version = "4.0.1"
 
   resource_id = azurerm_cosmosdb_account.db.id
 
