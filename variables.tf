9a67e35e022724d034a241319f4c178f841f46a7
ViacheslavSavinkov <viacheslav.savinkov@parsable.com>
2021-06-08 18:08:36 +0300

    ANLZ-1535: added ability to backup cosmosDB

--- variables.tf
+++ variables.tf
@@ -123,3 +123,9 @@ variable "logs_destinations_ids" {
   type        = list(string)
   description = "List of destination resources IDs for logs diagnostic destination. Can be Storage Account, Log Analytics Workspace and Event Hub. No more than one of each can be set."
 }
+
+variable "backup" {
+  type        = map(string)
+  default     = {}
+  description = "Backup block with type (Continuous / Periodic), interval_in_minutes and retention_in_hours keys"
+}
